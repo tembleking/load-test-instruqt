b4b2520af58830dd78e268acffd4a5101cf1ee42
Fede Barcelona <fede_rico_94@hotmail.com>
2025-04-21 11:48:15 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      overlays.default = final: prev: {
+        load-test-instruqt = prev.callPackage ./package.nix { };
+      };
+      flake = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          packages = with pkgs; {
+            inherit load-test-instruqt;
+            default = load-test-instruqt;
+          };
+          devShells.default =
+            with pkgs;
+            mkShell {
+              packages = [
+                python3
+                uv
+              ];
+            };
+
+          formatter = pkgs.nixfmt-rfc-style;
+        }
+      );
+    in
+    flake // { inherit overlays; };
+}
---
+++ package.nix
@@ -0,0 +1,23 @@
+{
+  python3Packages,
+}:
+let
+  pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+in
+python3Packages.buildPythonApplication {
+  pname = pyproject.project.name;
+  version = pyproject.project.version;
+  src = ./.;
+  pyproject = true;
+
+  build-system = with python3Packages; [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = with python3Packages; [
+    # your dependencies
+  ];
+
+  meta.mainProgram = "load-test-instruqt"; # Your script name to execute as command
+}
